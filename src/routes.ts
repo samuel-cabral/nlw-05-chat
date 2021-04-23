e637231bb4e49c62b65f7cd2a21954a9c45aa583
Samuel Cabral <samuelcabral.mail@gmail.com>
2021-04-23 08:36:06 -0300

    Creaete controller & route to messages

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
+import { MessagesController } from './controllers/MessagesController';
 import { SettingsController } from './controllers/SettingsController';
 import { UsersController } from './controllers/UsersController';
 
 const settingsController = new SettingsController();
 const usersController = new UsersController();
+const messagesController = new MessagesController();
 
 const routes = Router();
 
 routes.post('/settings', settingsController.create);
 routes.post('/users', usersController.create);
+routes.post('/messages', messagesController.create);
 
 export { routes };
