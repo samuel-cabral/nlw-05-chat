3852b4dcd28a2a6a93fc2ff9a22daa631c598603
Samuel Cabral <samuelcabral.mail@gmail.com>
2021-04-24 19:40:39 -0300

    Refactor the repository instantiation

--- src/routes.ts
+++ src/routes.ts
@@ -10,7 +10,10 @@ const messagesController = new MessagesController();
 const routes = Router();
 
 routes.post('/settings', settingsController.create);
+
 routes.post('/users', usersController.create);
+
 routes.post('/messages', messagesController.create);
+routes.get('/messages/:id', messagesController.showByUser);
 
 export { routes };
