dfdbbcb4e0f2e5edb6eda8d0aeb3cf0e5662dbe7
Samuel Cabral <samuelcabral.mail@gmail.com>
2021-04-25 22:39:29 -0300

    Set entity repository to connections

--- src/repositories/ConnectionsRepository.ts
+++ src/repositories/ConnectionsRepository.ts
@@ -1,7 +1,8 @@
-import { Repository } from 'typeorm';
+import { EntityRepository, Repository } from 'typeorm';
 
 import { Connection } from '../entities/Connection';
 
+@EntityRepository(Connection)
 class ConnectionsRepository extends Repository<Connection> {}
 
 export { ConnectionsRepository };
