0ed6ace200c6ac6090def7f4cab55ab3eb0f82a8
Samuel Cabral <samuelcabral.mail@gmail.com>
2021-04-25 21:43:49 -0300

    Refactor code of the server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,4 @@
-import express from 'express';
+import { http } from './http';
+import './websocket/client';
 
-import './database';
-import { routes } from './routes';
-
-const app = express();
-
-app.use(express.json());
-app.use(routes);
-
-app.listen(3333, () => console.log('Server is running on 3333 port!'));
+http.listen(3333, () => console.log('Server is running on 3333 port!'));
